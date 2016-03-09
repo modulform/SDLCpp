95962b8d3265883d1002044ed52c3a68a7f0de78
modulform <chris.rausch@gmx.net>
2016-03-09 22:57:30 +0100

    added timestamp to consoleHandler

--- SDLGame2D/SDLGame2D/consoleHandler.cpp
+++ SDLGame2D/SDLGame2D/consoleHandler.cpp
@@ -1,13 +1,36 @@
 #include "consoleHandler.h"
 
+std::string getCurrentTime()
+{
+	std::string helper = "";
+
+	time_t now = time(0);	//get current date/time (local system)
+	tm localTime;
+	localtime_s(&localTime, &now);
+	
+	helper.append(std::to_string(localTime.tm_hour));
+	helper.append(":");
+	helper.append(std::to_string(localTime.tm_min));
+	helper.append(":");
+	helper.append(std::to_string(localTime.tm_sec));
+	
+	return helper;
+}
+
 void logToConsole(char* msg, const char* details)
 {
+	time_t now = time(0);	//get current date/time (local system)
+	tm localTime;
+	localtime_s(&localTime, &now);
+
 	if (details != nullptr)
 	{
-		std::cout << msg << "(" << details << ")" << std::endl;
+		std::cout << std::setfill('0') << std::setw(2) << localTime.tm_hour << ":" << std::setw(2) << localTime.tm_min << ":" << std::setw(2) << localTime.tm_sec;
+		std::cout << " - " << msg << "(" << details << ")" << std::endl;
 	}
 	else
 	{
-		std::cout << msg << std::endl;
+		std::cout << std::setfill('0') << std::setw(2) << localTime.tm_hour << ":" << std::setw(2) << localTime.tm_min << ":" << std::setw(2) << localTime.tm_sec;
+		std::cout << " - " << msg << std::endl;
 	}
 }
--- SDLGame2D/SDLGame2D/consoleHandler.h
+++ SDLGame2D/SDLGame2D/consoleHandler.h
@@ -1,5 +1,9 @@
 #pragma once
 
 #include <iostream>
+#include <iomanip>
+#include <ctime>
+#include <string>
 
+std::string getCurrentTime();
 void logToConsole(char* msg, const char* details);
