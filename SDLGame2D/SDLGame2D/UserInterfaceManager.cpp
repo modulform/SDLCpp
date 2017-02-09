c9c4d103c8a9f62642ea0b6ac9ec009ad6badd3e
modulform <chris.rausch@gmx.net>
2017-02-09 21:43:01 +0100

    added comments

--- SDLGame2D/SDLGame2D/UserInterfaceManager.cpp
+++ SDLGame2D/SDLGame2D/UserInterfaceManager.cpp
@@ -6,6 +6,7 @@ UserInterfaceManager::UserInterfaceManager(SDL_Renderer* renderer)
 	iIdCounter = 0;			//initialize ID counter that provides session-unique IDs
 }
 
+//Render the list of UI objects
 void UserInterfaceManager::RenderObjects(SDL_Renderer* renderer)
 {
 	for (std::list<cUIObject*>::iterator it = objectList.begin(); it != objectList.end(); it++)
@@ -14,6 +15,7 @@ void UserInterfaceManager::RenderObjects(SDL_Renderer* renderer)
 	}
 }
 
+//Add an UI object with string value to the list of UI objects
 void UserInterfaceManager::addObject(std::string text, float positionX, float positionY)
 {
 	iIdCounter++;
@@ -22,6 +24,7 @@ void UserInterfaceManager::addObject(std::string text, float positionX, float po
 	objectList.push_back(temp);
 }
 
+//Add an UI object with float value to the list of UI objects
 void UserInterfaceManager::addObject(float* value, float positionX, float positionY)
 {
 	iIdCounter++;
